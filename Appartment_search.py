b13b374812a43993e4ce0c081af9c01eb4c27816
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-08-17 00:34:25 -0400

    function and variables are all there
    started implementing pseudocode

--- Appartment_search.py
+++ Appartment_search.py
@@ -7,6 +7,7 @@ import pandas as pd
 listing_counter = 0
 max_listing = 0
 
+
 # criteria
 
 Location = ""
@@ -16,17 +17,55 @@ max_price = 0
 minimum_bedroom = 0
 minimum_bathroom = 0
 number_of_listing_goal = 0
+Proximity_to = ""
 
 
-def find_total_page():
-    print("finding the number of option that match the search ")
-    return
+# page research result
+
+Address = ""  # Send this to the google map page to analyse
+price = 0
+Bedroom = 0
+Bathroom = 0
+Link = ""
+Finder = "Search_bot"
+Contact_info = 0
+Status = "To be check by human "
+Description = ""  # Send this to Chat Gpt Api to analyse
+
+
+# google map API result
+
+distance_from_uni = 0
+Transportation_type = ""
+Transportation_line = ""
+Transportation_station = ""
+
+# Chat GPT API result
+
+Parking_available = "?"
+Parking_spot = 0
+Parking_extra_price = 0
+Hot_water = "?"
+Heating = "?"
+Electricity = "?"
+Wifi = "?"
+A_C = "?"
+Appliances = "?"
+Gym = "?"
+Pool = "?"
+Spa = "?"
+Elevator = "?"
 
 
 def extract_criteria_from_excel():
     print(
         "extracting Location,goal,Type,max_price,minimum_bedroom,minimum_bathroom,number_of_listing_goal from the excel file"
     )
+
+
+def find_total_page():
+    print("finding the number of option that match the search ")
+    return
 
 
 def run_search():
@@ -76,4 +115,7 @@ def run_program():
 
     extract_criteria_from_excel()
 
-    run_search()
+    # run_search()
+
+
+run_program()
