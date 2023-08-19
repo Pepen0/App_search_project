060093e6c1d4d04b20249653731deee83f2985d0
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-08-19 00:22:44 -0400

    still can slide the price
    fixed and tested a bunch of bugs

--- Appartment_search.py
+++ Appartment_search.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 # Excel variable
@@ -19,6 +20,7 @@ Collumn_to_read = "B"
 Search_Appart_bot = webdriver.Chrome()
 listing_counter = 0
 max_listing = 0
+price_slider = []
 
 # criteria
 
@@ -26,6 +28,7 @@ Location = ""
 goal = ""
 Type = ""
 max_price = 0
+min_price = 0
 minimum_bedroom = 0
 minimum_bathroom = 0
 number_of_listing_goal = 0
@@ -82,10 +85,13 @@ def extract_criteria_from_excel():
         sheet_name=Criteria_sheet_title,
     )
 
+    print(len(Criteria_list))
+
     global Location
     global goal
     global Type
     global max_price
+    global min_price
     global minimum_bedroom
     global minimum_bathroom
     global number_of_listing_goal
@@ -95,38 +101,157 @@ def extract_criteria_from_excel():
     goal = Criteria_list.iloc[1, 0]
     Type = Criteria_list.iloc[2, 0]
     max_price = Criteria_list.iloc[3, 0]
-    minimum_bedroom = Criteria_list.iloc[4, 0]
-    minimum_bathroom = Criteria_list.iloc[5, 0]
-    number_of_listing_goal = Criteria_list.iloc[6, 0]
-    Proximity_to = Criteria_list.iloc[7, 0]
+    min_price = Criteria_list.iloc[4, 0]
+    minimum_bedroom = Criteria_list.iloc[5, 0]
+    minimum_bathroom = Criteria_list.iloc[6, 0]
+    number_of_listing_goal = Criteria_list.iloc[7, 0]
+    Proximity_to = Criteria_list.iloc[8, 0]
 
 
 def fill_search_page():
+    global price_slider
+    global min_price
+    global max_price
+
     print("entering info in page")
+
+    # change the location
 
     Search_Appart_bot.find_element(
         By.XPATH,
         "//input[@placeholder='Search by City, Neighbourhood, Region, Address or Centris N°']",
     ).send_keys(Location)
 
-    time.sleep(2)
+    time.sleep(1)
 
     Search_Appart_bot.find_element(
         By.XPATH,
         "//input[@placeholder='Search by City, Neighbourhood, Region, Address or Centris N°']",
     ).send_keys(Keys.ENTER)
 
-    time.sleep(5)
+    time.sleep(1)
 
-    # Click on the Select2 dropdown to expand it
-    select2_container = Search_Appart_bot.find_element(
+    # change the type
+
+    Type_select = Search_Appart_bot.find_element(
         By.CSS_SELECTOR, ".select2-selection.select2-selection--single"
     )
-    select2_container.click()
+    Type_select.click()
 
-    # Select the "For sale" option
-    option = Search_Appart_bot.find_element(By.XPATH, '//option[text()="For rent"]')
-    option.click()
+    time.sleep(1)
+
+    Type_option = Search_Appart_bot.find_element(
+        By.XPATH, '//option[text()="For rent"]'
+    )
+    Type_option.click()
+
+    time.sleep(1)
+
+    # Find where to slide
+
+    price_elements = Search_Appart_bot.find_elements(
+        By.XPATH, '//div[@id="RentPrice-slider-values"]/price'
+    )
+
+    for el in price_elements:
+        price_value = int(el.get_attribute("data-field-value-id"))
+        price_slider.append(price_value)
+
+    # select the price range
+
+    if (
+        min_price >= max_price
+    ):  # check that the min price is smaller then the max and fix it
+        min_price = max_price - 500
+
+    Price_select = Search_Appart_bot.find_element(
+        By.XPATH, "//button[@id='RentPrice-button']//span[contains(text(),'Price')]"
+    )
+    Price_select.click()
+
+    upper_bound_slide = Search_Appart_bot.find_element(
+        By.XPATH,
+        "//div[@class='primary-search-form search-section js-primary-form-other-field show-primary-filter']//div[6]",
+    )
+    lower_bound_slide = Search_Appart_bot.find_element(
+        By.XPATH,
+        "//div[@class='primary-search-form search-section js-primary-form-other-field show-primary-filter']//div[5]",
+    )
+    action_to_do = ActionChains(Search_Appart_bot)
+
+    time.sleep(1)
+
+    # apply minimum
+    Min_price_option = Search_Appart_bot.find_element(
+        By.XPATH,
+        "//div[@class='collapse-body']//input[@class='RentPrice-slider-min']",
+    )
+
+    closest_min_price = min(price_slider, key=lambda x: abs(x - min_price))
+
+    time.sleep(1)
+
+    # apply maximum
+    Max_price_option = Search_Appart_bot.find_element(
+        By.XPATH,
+        "//div[@class='collapse-body']//input[@class='RentPrice-slider-max']",
+    )
+
+    closest_max_price = min(price_slider, key=lambda x: abs(x - max_price))
+
+    time.sleep(5)
+
+    lower_bound = int(
+        Min_price_option.get_attribute("value").replace("$", "").replace(",", "")
+    )
+    upper_bound = int(
+        Max_price_option.get_attribute("value")
+        .replace("$", "")
+        .replace(",", "")
+        .replace("+", "")
+    )
+
+    timer = time.time()
+
+    while (lower_bound != closest_min_price) or (upper_bound != closest_max_price):
+        lower_bound = int(
+            Min_price_option.get_attribute("value").replace("$", "").replace(",", "")
+        )
+        upper_bound = int(
+            Max_price_option.get_attribute("value")
+            .replace("$", "")
+            .replace(",", "")
+            .replace("+", "")
+        )
+        time.sleep(1)
+
+        if int(lower_bound) < closest_min_price:
+            action_to_do.click_and_hold(lower_bound_slide).move_by_offset(
+                100, 0
+            ).release().perform()
+            print(">")
+
+        if int(upper_bound) > closest_max_price:
+            action_to_do.click_and_hold(upper_bound_slide).move_by_offset(
+                -100, 0
+            ).release().perform()
+            print("<")
+
+        print(
+            str(closest_min_price)
+            + " "
+            + str(closest_max_price)
+            + " "
+            + str(price_slider[0])
+            + " "
+            + str(price_slider[46])
+            + " "
+            + str(lower_bound)
+            + " "
+            + str(upper_bound)
+        )
+
+    print("good selection")
 
     time.sleep(5)
 
@@ -190,7 +315,7 @@ def run_program():
 
     fill_search_page()
 
-    # run_search()
+    run_search()
 
     Search_Appart_bot.quit()
 
